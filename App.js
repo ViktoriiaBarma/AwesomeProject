356b1143c87df18c4bb3e5a742c71fe0dfcae151
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-10-24 18:16:44 +0300

    add LoginScreen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StyleSheet, View } from 'react-native';
 
 
  export default function App() {
